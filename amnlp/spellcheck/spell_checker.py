00aae93c58f2540e7ca5f8148870de0b6e1b4996
Ey0s <eyosyasyosef1934@gmail.com>
2026-03-07 19:25:34 +0300

    add functions used for check spelling

---
+++ amnlp/spellcheck/spell_checker.py
@@ -0,0 +1,18 @@
+import os
+
+class SpellChecker:
+
+    def __init__(self):
+
+        path = os.path.join(
+            os.path.dirname(__file__),
+            "../resources/amharic_dict.txt"
+        )
+
+        with open(path, encoding="utf8") as f:
+
+            self.words = set(f.read().splitlines())
+
+    def check(self, word):
+
+        return word in self.words
