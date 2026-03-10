5592c3d01dcbda2bda4d644ea0eae9755ce37c0b
Ey0s <eyosyasyosef1934@gmail.com>
2026-03-10 20:41:43 +0300

    add more tags (person,organization,date)

--- amnlp/ner/ner_tagger.py
+++ amnlp/ner/ner_tagger.py
@@ -1,15 +1,24 @@
 class NERTagger:
-
-    locations = ["ኢትዮጵያ", "አዲስ", "አበባ"]
+    def __init__(self):
+        self.locations = ["ኢትዮጵያ", "አዲስ", "አበባ", "አሜሪካ", "ኬንያ"]
+        self.persons = ["አብይ", "መለስ", "ኃይለስላሴ"]
+        self.organizations = ["UN", "AU", "WHO"]
+        self.dates = ["ዛሬ", "ትላንት", "ነገ"]
 
     def recognize(self, tokens):
-
         entities = []
 
         for t in tokens:
-
             if t in self.locations:
-
                 entities.append((t, "LOCATION"))
+
+            elif t in self.persons:
+                entities.append((t, "PERSON"))
+
+            elif t in self.organizations:
+                entities.append((t, "ORG"))
+
+            elif t in self.dates:
+                entities.append((t, "DATE"))
 
         return entities
